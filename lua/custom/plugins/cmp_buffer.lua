950217e0f2ffa6a92d9e1989acb5fb0ad093df59
dretez <105381869+dretez@users.noreply.github.com>
2024-05-28 11:44:58 +0100

    Quick fix

---
+++ lua/custom/plugins/cmp_buffer.lua
@@ -0,0 +1,11 @@
+-- nvim-cmp source for buffer words.
+-- https://github.com/hrsh7th/cmp-buffer
+
+return {
+  'hrsh7th/cmp-buffer',
+  opts = {
+    sources = {
+      name = 'buffer',
+    },
+  },
+}
