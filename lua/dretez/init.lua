dc4f78978f960a365b5218b4daf45b7a8f412f54
dretez <andremalteznunes@sapo.pt>
2024-06-23 01:06:19 +0100

    Modularized config
    Split root init.lua into many individual components within /lua/
    directory

---
+++ lua/dretez/init.lua
@@ -0,0 +1,3 @@
+require 'dretez.lazy'
+require 'dretez.set'
+require 'dretez.remap'
---
+++ lua/dretez/plugins/status-line.lua
@@ -0,0 +1,5 @@
+return { -- Custom status line
+  'nvim-lualine/lualine.nvim',
+  opts = {},
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
