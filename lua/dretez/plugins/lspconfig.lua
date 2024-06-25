eb0946d605d9216818e7736161dab45a263afe9c
dretez <105381869+dretez@users.noreply.github.com>
2024-06-25 13:46:02 +0100

    Added LSPs

--- lua/dretez/plugins/lspconfig.lua
+++ lua/dretez/plugins/lspconfig.lua
@@ -127,6 +127,7 @@ return { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
+      clang_format = {},
       clangd = {},
       -- gopls = {},
       -- pyright = {},
@@ -140,7 +141,10 @@ return { -- LSP Configuration & Plugins
       tsserver = {},
       ast_grep = {},
       asm_lsp = {},
+      asmfmt = {},
       stylelint = {},
+      autopep8 = {},
+      prettier = {},
       lua_ls = {
         -- cmd = {...},
         -- filetypes = { ...},
