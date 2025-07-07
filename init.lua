b7e15c042399fecb47f95fd75b16d535ee367199
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-07-07 23:40:38 +0100

    feat: Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,1 @@
-HOME = os.getenv 'HOME'
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Set <space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require 'dretez.lazy'
-require 'dretez.set'
-require 'dretez.remap'
+require 'config'
